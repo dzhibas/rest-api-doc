a30cf355453684a873f50e6781b5e89ece04abd4
Nikolajus Krauklis <nikolajus@gmail.com>
2013-08-16 10:04:01 +0200

    added option arguments and demo documention output example

--- README.md
+++ README.md
@@ -9,4 +9,18 @@ Usage
 ==
 python parse.py
 
+Help
+==
+    usage: parse.py [-h] [-c] [-d DIR] [--filter FILTER] [-o OUTPUT]
+
+    optional arguments:
+      -h, --help            show this help message and exit
+      -c, --coverage        ouputs into stdin documentation coverage
+      -d DIR, --dir DIR     folder in which we need look for documentation
+                            annotation. default: test_src_folder/
+      --filter FILTER       gob filter in which files to search. default:
+                            *Controler*.php
+      -o OUTPUT, --output OUTPUT
+                            output filename. default: documentation.html
+
 open generated documentation.html
--- parse.py
+++ parse.py
@@ -13,16 +13,29 @@ from parserapi.config import modules as module_description
 from mako.lookup import TemplateLookup
 from mako import exceptions
 
+import argparse
+
+parser = argparse.ArgumentParser()
+parser.add_argument("-c", "--coverage", help="ouputs into stdin documentation coverage",
+                    action="store_true", default=False)
+parser.add_argument("-d", "--dir", help="folder in which we need look for documentation annotation. default: test_src_folder/",
+                    default="test_src_folder/")
+parser.add_argument("--filter", help="gob filter in which files to search. default: *Controler*.php",
+                    default="*Controller*.php")
+parser.add_argument("-o", "--output", help="output filename. default: documentation.html",
+                    default="documentation.html")
+args = parser.parse_args()
+
 template_lookup = TemplateLookup(directories=['templates/'],
-                                input_encoding='utf-8', output_encoding='utf-8',
-                                encoding_errors='replace')
+                                 input_encoding='utf-8', output_encoding='utf-8',
+                                 encoding_errors='replace')
 
 doc_template = template_lookup.get_template("doc.html")
 
 
 def get_controllers(folder):
     for root, dirnames, filenames in os.walk(folder):
-        for filename in fnmatch.filter(filenames, "*Controller*.php"):
+        for filename in fnmatch.filter(filenames, args.filter):
             yield os.path.join(root, filename)
 
 
@@ -55,7 +68,6 @@ def parse(folder):
     documentation_count = 0
 
     found_blocks = []
-
     for controller_filename in get_controllers(folder):
         f = codecs.open(controller_filename, "r", encoding='utf-8')
         code = "".join(f.readlines())
@@ -78,22 +90,22 @@ def parse(folder):
                     found_blocks.append(parsed_block)
                     documentation_count += 1
 
-    if methods_count > 0:
+    if args.coverage and methods_count > 0:
         print("Service documentation coverage")
         print("{0} service methods found and {1} documented. Coverage: {2}%".format(
-                        methods_count, documentation_count, documentation_count * 1. / methods_count * 100))
+              methods_count, documentation_count, documentation_count * 1. / methods_count * 100))
 
     return found_blocks
 
 
 if __name__ == "__main__":
-    blocks = parse("./test_src_folder/")
+    blocks = parse(args.dir)
     grouped_blocks = group_by_module(blocks)
     try:
-        output = doc_template.render(blocks=blocks, grouped_blocks=grouped_blocks, \
+        output = doc_template.render(blocks=blocks, grouped_blocks=grouped_blocks,
                                      when=datetime.today(), modules=module_description)
 
-        with codecs.open("documentation.html", "wb", encoding='utf-8') as f:
+        with codecs.open(args.output, "wb", encoding='utf-8') as f:
             f.write(output)
-    except: 
+    except:
         print(exceptions.text_error_template().render())
